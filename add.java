25406a9cd9acc49322d76548e6845665f717f5de
Meeran Siddiqui <mee.sidd.2000@gmail.com>
2023-04-03 14:26:03 +0530

    adding new add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+class addition{
+    public static void mian(String args[]){
+        int a;
+        int b;
+        a =10;
+        b=30;
+        System.out.println("The sum of the two number is :"+(a+b));
+    }
+}
