cc77d9276e6fb84a2f98105ddf82fd4a53cbe6b6
Meeran Siddiqui <mee.sidd.2000@gmail.com>
2023-04-03 14:08:55 +0530

    adding hey.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,11 @@
 class Diff{
+    public static void main(String args[])
+    {
+        int a,b;
+          a=80;
+          b=40;
+          System.out.println("The Difference of the two number:"+(a-b));
+          
+    }
     
 }
